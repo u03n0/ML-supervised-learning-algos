a2427793d11707ae758354f56ba7baff7056fcd4
u03n0 <u03n0@proton.me>
2024-03-06 10:45:35 +0100

    add more functions

--- src/utils.py
+++ src/utils.py
@@ -4,18 +4,31 @@ from pathlib import Path
 
 
 
-def save_data(df: pd.DataFrame, filename:str='clean.csv')-> Optional[None]:
-    """ Saves a Pandas DataFrame to '/data/processed/'.
-    Filename can be any name but must have a .csv extension.
+def save_data(df: pd.DataFrame, sub_folder: str)-> Optional[None]:
+    """ Saves a Pandas DataFrame to '/data/sub_folder/'
+    where subfolder can be 'processed', 'interim', or 'raw'
     """
+    full_path = create_abs_path(df, sub_folder)
+    df.to_csv(full_path, index=False)
+    return 
+    
+def build_filename(df: pd.DataFrame, prefix: str)-> str:
+    """ creates the appropriate file name for a df
+    """
+    return prefix + "_" + df.file_name
 
-    relative_path = "data/interim/"
+def check_if_exists(df: pd.DataFrame, sub_folder: str):
+    """ Determines if a csv file exists within /data/particular_sub_folder/
+    """
+    full_path = create_abs_path(df, sub_folder)
+    return full_path.exists()
+
+def create_abs_path(df, sub_folder):
+    """Builds an absolute path for a csv file
+    within /data/particular_sub_folder/
+    """
+    relative_path = f"data/{sub_folder}/"
     abs_path = Path(relative_path).resolve()
     abs_path.mkdir(parents=True, exist_ok=True)
-    full_path = abs_path / filename
-    _, ext = filename.split(".")
-    if ext.lower() == 'csv':
-        df.to_csv(full_path, index=False)
-        return 
-    else:
-        raise ValueError("File must have a .csv extension.")
+    return abs_path / build_filename(df, sub_folder)
+    
