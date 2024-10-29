5f2874030355579a5926eea477832e2eb9901f77
u03n0 <u03n0@proton.me>
2024-10-29 23:17:03 +0100

    moved functions out to main utils.py

--- naive_bayes/utils.py
+++ naive_bayes/utils.py
@@ -4,12 +4,6 @@ from collections import namedtuple
 from random import shuffle
 
 
-def build_dataset(path: str, nt: namedtuple)-> namedtuple:
-    """ Reads csv file and builds dataset as a list of namedtuples
-    """
-    with open(path, 'r') as file:
-        return [nt(*row) for row in csv.reader(file) if all(row)][1:]
-
 def train_test_split(dataset: List[namedtuple], ratio: float)-> Tuple[List[namedtuple]]:
     """ Simple train test split of a dataset,
     that is a namedtuple.
@@ -20,14 +14,6 @@ def train_test_split(dataset: List[namedtuple], ratio: float)-> Tuple[List[named
     train_data = dataset[:split_index]
     test_data = dataset[split_index:]
     return train_data, test_data
-
-def mul(l: List[float|int]):
-    """ Multiplies all elements in a list.
-    """
-    product = 1
-    for item in l:
-        product *= item
-    return product
 
 
 def clean_dataset(data: List[namedtuple], Nt: namedtuple)-> namedtuple:
