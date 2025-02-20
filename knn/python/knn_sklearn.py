ebb9aff679a8b3c528d30d14052dba01e94f6891
u03n0 <u03n0@proton.me>
2025-02-20 16:08:30 +0100

    added time

--- knn/python/knn_sklearn.py
+++ knn/python/knn_sklearn.py
@@ -1,12 +1,20 @@
+import time 
+import sys
+import pandas as pd 
+
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.feature_extraction.text import TfidfVectorizer
-import pandas as pd 
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parents[2]))
+
+from config import BASE_DIR, DATA_PATH
 
 
+start_time = time.time()
 # Read data into a Pandas DataFrame
-df = pd.read_csv("data/emails/email.csv")
+df = pd.read_csv(BASE_DIR / DATA_PATH / "emails" / "email.csv")
 # Select features (X) and labels (y)
 X = df['Message']
 y = df['Category']
@@ -17,10 +25,12 @@ X_vectorized = vectorizer.fit_transform(X)
 # Train test split
 X_train, X_test, y_train, y_test = train_test_split(X_vectorized, y)
 
-clf = KNeighborsClassifier(n_neighbors=5)
+clf = KNeighborsClassifier(n_neighbors=9)
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 print(accuracy_score(y_test, y_pred))
 
-
+end_time: float = time.time()
+execution_time: float = end_time - start_time
+print(f"Execution time: {execution_time} milliseconds")
 
