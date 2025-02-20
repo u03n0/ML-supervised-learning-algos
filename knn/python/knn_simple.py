6c526b3ce31343b31a0fd39d617103c1eea37410
u03n0 <u03n0@proton.me>
2025-02-20 15:08:02 +0100

    added tie breaker and increased k to 9

--- knn/python/knn_simple.py
+++ knn/python/knn_simple.py
@@ -1,6 +1,6 @@
 import sys
 import time
-
+import random
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[2]))
 from utils.py_utils  import build_dataset, train_test_split, clean_dataset, get_tf_idf, cosine_similarity
@@ -13,15 +13,15 @@ def classify_point(dataset: list[dict[str, list[float]]], point: list[float], k:
     """ Finds the top K cosine similarities from the point to all other
     points in the dataset. A point is a vector representation of a document.
     """
-    distance = []
+    distance: list = []
     for dict_ in dataset:
         for label, vector in dict_.items():
             dist = cosine_similarity(point, vector)
             distance.append((dist, label))
 
     distance = sorted(distance)[:k]
-    freq1 = 0 
-    freq2 = 0 
+    freq1: int = 0 
+    freq2: int = 0 
 
     for item in distance:
         _, label = item
@@ -29,6 +29,8 @@ def classify_point(dataset: list[dict[str, list[float]]], point: list[float], k:
             freq1 += 1 
         elif label == 'spam':
             freq2 += 1 
+    if freq1 == freq2:
+        return random.choice(["ham", "spam"])
     return 'ham' if freq1 > freq2 else "spam"
 
 
@@ -39,16 +41,16 @@ with open(Path(BASE_DIR / DATA_PATH / "stopwords_en.txt"), 'r') as file:
 
 path_to_data: Path = BASE_DIR / DATA_PATH / "emails" / "email.csv"
 dataset = build_dataset(path_to_data)
-clean = clean_dataset(dataset[:300])
+clean = clean_dataset(dataset[:200])
 encoded_data = get_tf_idf(clean)
 # Train test split
-train_data, test_data = train_test_split(encoded_data, 0.6)
+train_data, test_data = train_test_split(encoded_data, 0.8)
 # Make predictions
 correct: int = 0
 for dict_ in test_data:
     for label, vector in dict_.items():
         y_pred = label
-        y_hat = classify_point(train_data, vector, k=5)
+        y_hat = classify_point(train_data, vector, k=9)
         if y_hat == y_pred:
             correct += 1 
 
--- knn/python/knn_simple_oop.py
+++ knn/python/knn_simple_oop.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import random
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[2]))
 from utils.py_utils import build_dataset,train_test_split, clean_dataset, get_tf_idf, cosine_similarity
@@ -9,16 +10,19 @@ from config import BASE_DIR, DATA_PATH
 start_time = time.time()
 
 class KNN():
-
+    """
+    K-Nearest Neighbor Class.
+    """
     def __init__(self, points, k):
         self.k = k
         self.points = points
 
     def classify_point(self,  point):
-        """
-
+        """ Computes the distance between a point and all other vectors.
+        Returning the class based on frequency found within the 'k' most 
+        closet distances.
         """ 
-        distance = []
+        distance: list = []
 
         for dict_ in self.points:
             for label, vector in dict_.items():
@@ -26,26 +30,28 @@ class KNN():
                 distance.append((dist, label))
 
         distance = sorted(distance)[:self.k]
-        freq1 = 0 
-        freq2 = 0 
+        freq1: int = 0 
+        freq2: int = 0 
 
         for d in distance:
             if d[1] == 'ham':
                 freq1 += 1 
             elif d[1] == 'spam':
                 freq2 += 1 
+        if freq1 == freq2:
+            return random.choice(["ham", "spam"])
         return 'ham' if freq1 > freq2 else "spam"
 
 
 
 path_to_data = BASE_DIR / DATA_PATH / "emails" / "email.csv"
 dataset = build_dataset(path_to_data)
-cleaned_dataset = clean_dataset(dataset[:300])
+cleaned_dataset = clean_dataset(dataset[:200])
 encoded_data = get_tf_idf(cleaned_dataset)
 
 train_data, test_data = train_test_split(encoded_data, 0.8)
 
-clf = KNN(train_data, k=5)
+clf = KNN(train_data, k=9)
 correct = 0
 
 for dict_ in test_data:
